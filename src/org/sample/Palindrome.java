248f279458fae34e52f07af0d8c4ed976377fb3f
Kamalakannan <kamalakkannan_rajamanickam@trimble.com>
2019-04-26 08:20:17 +0530

    Added palindrome series

---
+++ src/org/sample/Palindrome.java
@@ -0,0 +1,10 @@
+package org.sample;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Created a class for palindrome series");
+	}
+
+}
